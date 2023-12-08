b6df0cbbd97a73ab2ead11c5f05773c07d07954e
Edson Junior <95450378+EdsoonJr@users.noreply.github.com>
2023-12-08 21:55:45 +0000

    pag entrar

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import Cadastrar from "./components/tela/Cadastrar";
-import Entrar from "./Entrar";
+import Entrar from "./components/entrar/Entrar";
 
 const App = () => {
   return (
@@ -11,7 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cadastrar" element={<Cadastrar />} />
-          <Route path="/entrar" element={<Cadastrar />} />
+          <Route path="/entrar" element={<Entrar />} />
         </Routes>
       </div>
     </Router>
---
+++ src/components/entrar/Entrar.jsx
@@ -0,0 +1,15 @@
+// Entrar.js
+import React from "react";
+import Header from "../header/Header";
+
+const Entrar = () => {
+  return (
+    <div>
+      <Header />
+      <h1>Página em Desenvolvimento - Entrar</h1>
+      <h1>Esta página ainda está em construção. Em breve estará disponível!</h1>
+    </div>
+  );
+};
+
+export default Entrar;
